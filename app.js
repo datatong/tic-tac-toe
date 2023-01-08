037d74ccad5f8a011a4be50afc780b457fc2fe16
datatong <59157188+datatong@users.noreply.github.com>
2023-01-07 22:39:48 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+////STORE THE GAMEBOARD AS AN ARRAY INSIDE OF A GAMEBOARD OBJECT
+
+//module (one of each) for gameboard & displaycontroller
+//factory functions (multiples of) for players
+
+//function that will render the contents of the gameboard array to the webpage
+
+//functions that allow players to add marks to a spot on the board
+//then tie it to DOM, letting players click on gameboard to place marker
+
+//build logic to check when game is over
+
+//allow player to put in their names, include start/restart button
+//add display element that congratulates the winner
+
+//////---- START BELOW ----//////

e51cb4c3317a084f6f272989145ba01cfacdda4a
datatong <59157188+datatong@users.noreply.github.com>
2023-02-05 16:44:14 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -33,13 +33,15 @@ const gameBoardModule = (function() {
     ];
 
     let winner = null;
-    let turn = 0;
+    let turnCounter = 0;
     let winnerCombo = [];
 
     //ToDo: game state validation
     let winCheck = () => {
-        //ToDo: check for winning combos or determine a tie if board is full
-        //ToDo: else continue the game
+        //ToDo: check for winning combos btwn player X and O 
+        //OR determine a tie if turnCounter is 9
+        //ELSE continue the game
+        turnCounter++;
     }
 
     return {gameBoardXO, winnerCombo, winner, winCheck};
@@ -54,23 +56,23 @@ const displayControllerModule = (() => {
 
 //player factory function
 const Player = (name, symbolXO, turn) => {
-    //what can players do
+    //player actions
     const placeSymbol = (function () {
-        const field = document.querySelectorAll(".field");
+        const field = document.querySelectorAll('.field');
         field.forEach(field => {
             field.addEventListener('click', e => {
                 if (player1.turn == true && gameBoardModule.winner == null && e.target.textContent == '') {
-                    gameBoardXO[e.target.index] = player1.symbolXO;
+                    gameBoardModule.gameBoardXO[e.target.id] = player1.symbolXO;
                     field.textContent = player1.symbolXO;
                     player1.turn = false;
                     player2.turn = true;
-                    console.log("Player1 made a move: " + gameBoardXO)
+                    console.log("Player1 made a move: " + gameBoardModule.gameBoardXO);
                 } else if (player2.turn == true && gameBoardModule.winner == null && e.target.textContent == '') {
-                    gameBoardXO[e.target.index] = player2.symbolXO;
+                    gameBoardModule.gameBoardXO[e.target.id] = player2.symbolXO;
                     field.textContent = player2.symbolXO;
                     player1.turn = true;
                     player2.turn = false;
-                    console.log("Player2 made a move: " + gameBoardXO) 
+                    console.log("Player2 made a move: " + gameBoardModule.gameBoardXO);
                 } else {
                     return;
                 };
@@ -86,6 +88,3 @@ const Player = (name, symbolXO, turn) => {
 //create player objects
 const player1 = Player('Player 1', 'X', true);
 const player2 = Player('Player 2', 'O', false);
-
-////DOM SELECTORS////
-
